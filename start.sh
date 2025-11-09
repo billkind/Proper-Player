0406c296085d1abc4148e7f9a5c59c7810e7f6d1
billkind <155904710+billkind@users.noreply.github.com>
2025-11-09 20:32:47 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Installer les dépendances système
-apt-get update && apt-get install -y ffmpeg
-
-# Démarrer l'application
-exec uvicorn forapitest:app --host 0.0.0.0 --port ${PORT:-8000}
